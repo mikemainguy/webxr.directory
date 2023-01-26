f11334a2e96acc0d33eb765f53f19319f5be1b26
Michael Mainguy <mike.mainguy@gmail.com>
2023-01-26 14:26:06 -0600

    Added indexing key to .env.  enabled upload to algolia.

--- pages/api/add.js
+++ pages/api/add.js
@@ -1,6 +1,20 @@
+const algoliasearch = require('algoliasearch');
+require('dotenv').config();
+const crypto=require('crypto');
+
+
+const client = algoliasearch(process.env.ALGOLIA_APP_KEY, process.env.ALGOLIA_ADMIN_KEY);
+const index = client.initIndex('your_index_name');
 export default function handler(req, res) {
     if (req.method === 'POST') {
-        console.log(JSON.stringify(req.body))
+        //index.saveObject(req.body);
+        const data = req.body;
+        const hash = crypto.createHash('md5').update(req.body.url).digest('hex');
+        data.objectID = hash;
+        data.timestamp = Date.now().valueOf();
+
+        index.saveObject(req.body);
+        console.log(JSON.stringify(data));
         res.redirect('/');
     } else {
 
