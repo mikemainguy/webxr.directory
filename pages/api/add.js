2554954d8cb71e8f859a87944f79dc3497bf63cb
Michael Mainguy <mike.mainguy@gmail.com>
2023-01-26 16:15:36 -0600

    Added debug message to api.

--- pages/api/add.js
+++ pages/api/add.js
@@ -5,7 +5,7 @@ const crypto=require('crypto');
 
 const client = algoliasearch(process.env.ALGOLIA_APP_KEY, process.env.ALGOLIA_ADMIN_KEY);
 const index = client.initIndex('xrdirectory');
-export default function handler(req, res) {
+export default async function handler(req, res) {
     if (req.method === 'POST') {
         //index.saveObject(req.body);
         const data = req.body;
@@ -13,7 +13,8 @@ export default function handler(req, res) {
         data.objectID = hash;
         data.timestamp = Date.now().valueOf();
 
-        index.saveObject(req.body);
+        const resp = await index.saveObject(req.body);
+        console.log(JSON.stringify(resp));
         console.log(JSON.stringify(data));
         res.redirect(303, '/');
     } else {
